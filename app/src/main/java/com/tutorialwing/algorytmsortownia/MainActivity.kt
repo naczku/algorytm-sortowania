349ed67f9a32391255319ee6b65733226b6a66c1
naczku <damiannaczk1@gmail.com>
2023-01-05 07:04:35 +0100

    progres bar, losowanie  i algorytm sortowania poprawnie zaimplementowane

--- app/src/main/java/com/tutorialwing/algorytmsortownia/MainActivity.kt
+++ app/src/main/java/com/tutorialwing/algorytmsortownia/MainActivity.kt
@@ -1,12 +1,13 @@
 package com.tutorialwing.algorytmsortownia
 
 import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
-import android.widget.TextView
-import android.os.Bundle
 import android.widget.ProgressBar
-import java.util.*
+import android.widget.TextView
+import kotlin.math.sin
+import kotlin.random.Random
 
 
 class MainActivity : AppCompatActivity() {
@@ -14,59 +15,72 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val edit = findViewById<EditText>(R.id.input1)
-        val text = findViewById<TextView>(R.id.wypisz1)
         var arrNums: MutableList<Int> = mutableListOf()
 
         findViewById<Button>(R.id.losuj).setOnClickListener {
-            val rand = Random()
-            text.text=""
-            for (int in 1 ..  edit.text.toString().toInt()) {
-                val a= rand.nextInt(edit.text.toString().toInt())
-                text.append(a.toString())
+            findViewById<ProgressBar>(R.id.progres).progress = 0
+            val numAmount = findViewById<EditText>(R.id.input1).text.toString().toInt() - 1
+            arrNums.clear()
 
+            for (i in 0..numAmount) {
+
+                val randed = (0..1000).random()
+                arrNums += randed
+
+                findViewById<TextView>(R.id.wypisz1).text = ""
+                for (element in arrNums) {
+                    findViewById<TextView>(R.id.wypisz1).append(element.toString() + " ")
+                }
             }
         }
+
         findViewById<Button>(R.id.sortuj).setOnClickListener {
 
-            findViewById<ProgressBar>(R.id.pbProgress).progress = 0
-            findViewById<ProgressBar>(R.id.pbProgress).max = arrNums.size
+            findViewById<ProgressBar>(R.id.progres).progress = 0
+            findViewById<ProgressBar>(R.id.progres).max = arrNums.size
 
             var arrNumsSorted: List<Int> = quicksort(arrNums)
 
-            findViewById<TextView>(R.id.input1).text = ""
+            findViewById<TextView>(R.id.wypisz1).text = ""
             for (element in arrNumsSorted) {
-                findViewById<TextView>(R.id.tvNumbers).append(element.toString()+" ")
+                findViewById<TextView>(R.id.wypisz1).append(element.toString()+" ")
             }
         }
 
     }
 
-    fun quicksort(items: List<Int>): List<Int> {
-        if (items.size <= 1) {
-            return items
-        }
 
-        val chosenItem: Int = items[items.size / 2]
-
-        val smallerList: MutableList<Int> = mutableListOf()
-        val equalList: MutableList<Int> = mutableListOf()
-        val greaterList: MutableList<Int> = mutableListOf()
-        items.forEach {
-            when {
-                it < chosenItem -> {smallerList.add(it) ; findViewById<ProgressBar>(R.id.pbProgress).progress += 1}
-                it > chosenItem -> {greaterList.add(it) ; findViewById<ProgressBar>(R.id.pbProgress).progress += 1}
-                else -> {equalList.add(it) ; findViewById<ProgressBar>(R.id.pbProgress).progress += 1}
+        fun quicksort(items: List<Int>): List<Int> {
+            if (items.size <= 1) {
+                return items
             }
+
+            val chosenItem: Int = items[items.size / 2]
+
+            val smallerList: MutableList<Int> = mutableListOf()
+            val equalList: MutableList<Int> = mutableListOf()
+            val greaterList: MutableList<Int> = mutableListOf()
+            items.forEach {
+                when {
+                    it < chosenItem -> {
+                        smallerList.add(it); findViewById<ProgressBar>(R.id.progres).progress += 1
+                    }
+                    it > chosenItem -> {
+                        greaterList.add(it); findViewById<ProgressBar>(R.id.progres).progress += 1
+                    }
+                    else -> {
+                        equalList.add(it); findViewById<ProgressBar>(R.id.progres).progress += 1
+                    }
+                }
+            }
+
+            val sortedList: MutableList<Int> = mutableListOf()
+            sortedList.addAll(quicksort(smallerList))
+            sortedList.addAll(equalList)
+            sortedList.addAll(quicksort(greaterList))
+            return sortedList
         }
 
-        val sortedList: MutableList<Int> = mutableListOf()
-        sortedList.addAll(quicksort(smallerList))
-        sortedList.addAll(equalList)
-        sortedList.addAll(quicksort(greaterList))
-        return sortedList
     }
 
-}
-    }
-}
+
